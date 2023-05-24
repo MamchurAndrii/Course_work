ed68998bdca4f48aa7744e16edc312680d177c8b
Andrii Mamchur <118045904+Mamchur11@users.noreply.github.com>
2023-05-24 22:06:20 +0300

    Initial commit-2

--- src/main/java/com/yan/crm_project/CrmProjApplication.java
+++ src/main/java/com/yan/crm_project/CrmProjApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.*;
 @SpringBootApplication
 public class CrmProjApplication {
     public static void main(String[] args) {
-
-
          SpringApplication.run(CrmProjApplication.class, args);
-
 
     }
 }
